92e494bf3703d8b75cc774e133a11c6f074918a4
Gamson3 <gamson3@yahoo.com>
2025-03-20 10:26:45 +0100

    Update Experience Details

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,15 +19,14 @@ export const Experiences = [
     description: `Provided technical support for software solutions, improving client satisfaction by resolving issues and 
 communicating product benefits effectively. Developing strong communication skills essential for cross-team 
 collaboration, enhancing my ability to explain solutions clearly and effectively.`,
-    technologies: ["HTML", "CSS", "Vue.js", "MySQL"]
+    technologies: ["CRM Software", "Excel", "Cross-Team Collaboration", "Technical Communication"]
 },{
     role: "Customer Service Specialist",
     company: "Alliance Health, USA",
     year: "July 2021 - Jan 2022 ",
-    description: `Assisted clients with 100+ technical inquiries, enhancing attention to detail, analytical skills and building a 
-strong foundation in debugging and problem-solving. While coordinating with health professionals to ensure 
-seamless service`,
-    technologies: ["HTML", "CSS", "Vue.js", "MySQL"]
+    description: `Assisted clients with over 100+ technical inquiries, enhancing attention to detail, analytical skills and building a 
+strong foundation in debugging and problem-solving. While coordinating with health professionals to streamline service delivery and enhance user experience.`,
+    technologies: ["CRM Software", "Excel", "Attention to Detail", "Problem Solving"]
 },];
 
 
